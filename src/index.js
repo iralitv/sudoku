0ba37835da71c538ce755ebb3a8a614189cf54a3
Iralitv <ira.litvinchuk@mifort.org>
2019-10-16 00:23:42 +0300

    feat: add check for row, column, 3x3Square

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
-}
+
+
+
+};
+
+const checkValue = function(matrix, column, row, value){
+  if(checkRow(matrix, row, value) && checkColumn(matrix, column, value) && checkSquare(matrix, column, row, value)){
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const checkRow = function(matrix, row, value) {
+  for(let i = 0; i < matrix[row].length; i++){
+    if(matrix[row][i] === value){
+      return false;
+    }
+  }
+  return true;
+};
+
+const checkColumn = function(matrix, col, value) {
+  for(let i = 0; i < matrix.length; i++){
+    if(matrix[i][col] === value){
+      return false;
+    }
+  }
+  return true;
+};
+
+const checkSquare = function(matrix, column, row, value) {
+  for(let i = Math.floor(row / 3) * 3; i < Math.floor(row / 3) * 3 + 3; i++) {
+    for(let j = Math.floor(column / 3) * 3; j < Math.floor(column / 3) * 3 + 3; j++) {
+      if(matrix[i][j] === value) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+
